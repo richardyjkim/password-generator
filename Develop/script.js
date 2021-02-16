62f1425d5c95382236172672331ef12ee664e2b7
Richard <richardyjkim@gmail.com>
2021-02-15 17:23:41 -0800

    adjusted prompt when button is clicked

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,6 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-// Setting
-let pLength = Number(prompt ("How many characters would you like your password to be?"));
-let lowerCase = "abcdefghijklmnopqrstuvwxyz"
-let upperCase = lowerCase.toUpperCase();
-let number = "1234567890"
-let special = "~!@#$%^&*()_+"
-let cap = false;
-let low = false;
-let num = false;
-let spec = false;
-let firstTime = true;
-
-// Password Intro
-while (isNaN(pLength) ||pLength < 8 || pLength > 128)
-  pLength = Number(prompt("Length must be 8-128 characters!"));
-
-while (!cap && !low && !num && !spec) {
-  if (!firstTime) {
-    alert("You must select at least one charater type!");
-  }
-  
-  cap = confirm("Would you like to use uppercase letters?");
-  low = confirm("Would you like to use lowercase letters?");
-  num = confirm("Would you like to use numbers?");
-  spec = confirm("Would you like to use special characters?");
-  firstTime = false;
-}
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
@@ -38,6 +11,33 @@ function writePassword() {
 
 // generate password
 function generatePassword() {
+  // Setting
+  let pLength = Number(prompt ("How many characters would you like your password to be?"));
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  let upperCase = lowerCase.toUpperCase();
+  let number = "1234567890"
+  let special = "~!@#$%^&*()_+"
+  let cap = false;
+  let low = false;
+  let num = false;
+  let spec = false;
+  let firstTime = true;
+
+  // Password Intro
+  while (isNaN(pLength) ||pLength < 8 || pLength > 128)
+    pLength = Number(prompt("Length must be 8-128 characters!"));
+
+  while (!cap && !low && !num && !spec) {
+    if (!firstTime) {
+      alert("You must select at least one charater type!");
+    }
+    
+    cap = confirm("Would you like to use uppercase letters?");
+    low = confirm("Would you like to use lowercase letters?");
+    num = confirm("Would you like to use numbers?");
+    spec = confirm("Would you like to use special characters?");
+    firstTime = false;
+  }
   let letters = [];
   for (let i = 0; i < 4 ; i++) {
     if (cap && !letters.includes(upperCase)) {
